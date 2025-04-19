8c90bfdc8ac4b45be02627b1a9989a7602b18aab
Noceur <bernardglenn.casera@evsu.edu.ph>
2025-04-20 01:36:48 +0800

    added auth routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+})->name('index');
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+
+    Route::get('/signup', [AuthController::class, 'signup'])->name('signup');
+
+    Route::get('/forgot-password', [AuthController::class, 'forgotPass'])->name('forgotPass');
 });
