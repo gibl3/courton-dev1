011fa19d89c02941011dbc11a6a5646f86735738
Noceur <bernardglenn.casera@evsu.edu.ph>
2025-04-20 01:37:01 +0800

    added auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function signup()
+    {
+        return view('auth.signup');
+    }
+
+    public function forgotPass()
+    {
+        return view('auth.forgotPass');
+    }
+}
