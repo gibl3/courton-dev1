e0b9f6fd3163cbeeae6baf32cbf29e17047f8290
Noceur <bernardglenn.casera@evsu.edu.ph>
2025-05-30 18:25:35 +0800

    implement rate limiting for login attempts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Http\Request; // <-- Correct import
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('login', function (Request $request) {
+            return Limit::perMinute(3, 5)
+                ->by($request->input('email') . '|' . $request->ip())
+                ->response(function (Request $request, array $headers) {
+                    return response()->json([
+                        $retryAfter = $headers['Retry-After'],
+                        'message' => 'Too many login attempts',
+                        'retry_after' => $retryAfter,
+                        'available_in' => $retryAfter
+                    ], 429, $headers);
+                });
+        });
     }
 }
